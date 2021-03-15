9bca41e0e38ab616ac772d6d1edc695c5371fe2b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-14 19:37:08 -0500

    commit tree 3c5363e701ab35f79e33186ec7c269f50551b178

--- app.rb
+++ app.rb
@@ -7,5 +7,9 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food' do
+    params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 
 end
